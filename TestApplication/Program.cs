3e871575aa5b5c4197db384167cdac4bc15c52fb
Orangera1n <96156354+Orangera1n@users.noreply.github.com>
2021-12-26 22:30:39 -0500

    Add project files.

---
+++ TestApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("7971");
+            Console.WriteLine("Buy A Mac");
+        }
+    }
+}
